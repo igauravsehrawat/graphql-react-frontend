60fe475b992bba2ca59d7abcbd8e2103a1efa0a0
G <igauravsehrawat@gmail.com>
2018-11-28 20:18:47 +0530

    --add: Using ThemeProvider from styled components.

--- components/Page.js
+++ components/Page.js
@@ -2,31 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+
+const theme = {
+  red: '#FA0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px rgba(0, 0, 0, 0.01)',
+}
 
 const StyledPage = styled.div`
-  background-color: white;
-  color: black;
+  background-color: ${props => props.theme.offWhite};
+  color: ${props => props.theme.black};
 `
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
-  background-color: red;
+  background-color: ${props => props.theme.red};
 `
-
 class Page extends Component {
   render() {
     return (
-      <StyledPage>
-        <Meta />
-        <p>This is coming from Page.</p>
-        <Header />
-        <Inner>
-          {this.props.children}
-        </Inner>
-      </StyledPage>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <p>This is coming from Page.</p>
+          <Header />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
