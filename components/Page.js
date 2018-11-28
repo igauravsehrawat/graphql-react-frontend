45a89c4b4da77bab54f726b057bef2a63c42455b
G <igauravsehrawat@gmail.com>
2018-11-28 17:31:18 +0530

    --add: getting started with styled component theme and layout.

--- components/Page.js
+++ components/Page.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  background-color: white;
+  color: black;
+`
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: red;
+`
 
 class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <p>This is coming from Page.</p>
         <Header />
-        {this.props.children}
-      </div>
+        <Inner>
+          {this.props.children}
+        </Inner>
+      </StyledPage>
     );
   }
 }
