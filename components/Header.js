e76f2e171b40c874d2111d7d806550e6c9df48e2
G <igauravsehrawat@gmail.com>
2018-11-30 17:26:08 +0530

    --add: Header added in center.

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,24 @@
 import Nav from './Nav';
+import styled from 'styled-components'
+import Link from 'next/link';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-decoration: none;
+  }
+  @media (max-width: 1500px) {
+      margin: 0;
+      text-align: center;
+  }
+`;
 
 const Header = () => {
   return (
@@ -7,7 +27,11 @@ const Header = () => {
         <img src='' alt='logo' />
       </div>
       <div className="bar">
-        <a href="">Dev Fits</a>
+        <Logo>
+          <Link href="/">
+            <a>Dev Fits</a>
+          </Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
