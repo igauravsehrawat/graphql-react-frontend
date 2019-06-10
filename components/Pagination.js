bcdcc549c457c213538574a8e2ece67a20c41dc7
G <igauravsehrawat@gmail.com>
2019-06-10 13:25:52 +0200

    --wip: pagination skeleton.

---
+++ components/Pagination.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Link from 'next/link';
+import Error from './ErrorMessage';
+import PaginationStyles from './styles/PaginationStyles';
+import { perPage } from '../config';
+
+const ITEMS_CONNECTION_QUERY = gql`
+  query ITEMS_CONNECTION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`
+
+const Pagination = props => {
+  return (
+    <Query query={ITEMS_CONNECTION_QUERY}>
+    {({data, loading, error}) => {
+      if (error) return <Error error={error} />
+      console.log({data});
+      const totalPages = parseFloat(data.itemsConnection.aggregate.count/perPage);
+      const page = props.page;
+      if (loading) return <p>Loading...</p>
+      return (
+      <PaginationStyles>
+        <Link prefetch href={{
+              pathname: 'items',
+              query: { page: props.page - 1 }
+            }}>
+          <a class="prev">◀◀ Prev</a>
+        </Link>
+        <p>{page} of {totalPages}</p>
+        <p>Total item {data.aggregate.count}</p>\
+        <Link prefetch href={{
+            pathname: 'items',
+            query: { page: props.page + 1 }
+          }}>
+            <a class="next">>> Next</a>
+          </Link>
+      </PaginationStyles>
+        )
+      }}
+    </Query>
+  )
+}
+
+export default Pagination;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Items from '../components/Items';
 const Home = props => {
   return (
     <div>
-      <Items />
+      <Items page={props.query.page || 1} />
     </div>
   )
 }
