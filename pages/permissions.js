0afc6a56f4ee30277ba19ba7af62b92217500680
G <igauravsehrawat@gmail.com>
2020-01-12 20:02:00 +0100

    --wip: permissions.

---
+++ pages/permissions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PermissionsPage extends Component {
+  render() {
+    return (
+      <p>Permissions</p>
+    )
+  }
+}
+
+export default PermissionsPage;
