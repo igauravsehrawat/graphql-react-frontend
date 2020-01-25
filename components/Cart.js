90b9f9bb5391da3ca7cf99bbade689971bc807ee
G <igauravsehrawat@gmail.com>
2020-01-25 20:23:11 +0100

    --add: Cart toggle.

--- components/Cart.js
+++ components/Cart.js
@@ -7,34 +7,40 @@ import CartStyles from './styles/CartStyles';
 import SickButton from './styles/SickButton';
 
 export const LOCAL_STATE_QUERY  = gql`
-  query getCartState {
+  query {
     cartOpen @client
   }
 `;
 
 export const TOGGLE_CART_MUTATION = gql`
   mutation toggleCart {
+    toggleCart @client {
+      cartOpen,
+    }
   }
 `;
 
 export default class Cart extends Component {
   render(props) {
-    return <Query query={LOCAL_STATE_QUERY}>
-      {({data, loading, error}) => {
-        {console.log('data is...', data)};
-        return <CartStyles open={data.cartOpen}>
-        <header>
-          <CloseButton title="close">&times;</CloseButton>
-          <Supreme>Your Cart</Supreme>
-          <p>You have __ no of items in your cart.</p>
-        </header>
-        <footer>
-          <p>Your total is</p>
-          <SickButton>Checkout</SickButton>
-        </footer>
-      </CartStyles>
-    }}
-    </Query>
-
+    return <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {(toggleCart) => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({data, loading, error}) => {
+            {console.log('data is...', data)};
+            return <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You have __ no of items in your cart.</p>
+            </header>
+            <footer>
+              <p>Your total is</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
+      }}
+      </Query>
+      )}
+    </Mutation>
   }
 }
--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
 import NavStyles from './styles/NavStyles';
 import User from '../components/User';
 import Signout from '../components/Signout';
+import { TOGGLE_CART_MUTATION } from '../components/Cart';
 
 const Nav = () => (
 
@@ -31,6 +33,11 @@ const Nav = () => (
           <a>Shop</a>
         </Link>
         <Signout/>
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {(toggleCart) => (
+            <button onClick={toggleCart}>My Cart</button>
+          )}
+        </Mutation>
       </NavStyles>
     )}
     </User>
