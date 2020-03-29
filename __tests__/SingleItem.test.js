18664309fef772343674e4f4e739d76a01252e22
G <igauravsehrawat@gmail.com>
2020-03-29 19:52:09 +0200

    --add: mount tests.

---
+++ __tests__/SingleItem.test.js
@@ -0,0 +1,59 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+import SingleItem, { SINGLE_ITEM_QUERY } from '../components/SingleItem';
+import { fakeItem } from '../lib/testUtils';
+
+describe('<SingleItem/>', () => {
+  it('renders with proper data', async () => {
+    //  Invariant Violation: Could not find "client" in the context or
+    // passed in as a prop. Wrap the root component in an <ApolloProvider>,
+    // or pass an ApolloClient instance in via props.
+    const mocks = [
+      {
+        // when someone makes a rreqruest with this query and variable combo
+        request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+        result: {
+          data: {
+            item: fakeItem(),
+          },
+        },
+        // return this fake data (mocked data)
+      },
+    ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>
+    );
+    expect(wrapper.text()).toContain('Loading...');
+    await wait();
+    wrapper.update();
+    console.log(wrapper.debug());
+    expect(toJSON(wrapper.find('h2'))).toMatchSnapshot();
+    expect(toJSON(wrapper.find('img'))).toMatchSnapshot();
+    expect(toJSON(wrapper.find('p'))).toMatchSnapshot();
+  });
+
+  it('Errors with a not found item', async () => {
+    const mocks = [
+      {
+        request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+        result: {
+          errors: [new Error('Items Not Found!')],
+        },
+      },
+    ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    const item = wrapper.find('[data-test="graphql-error"]');
+    expect(item.text()).toContain('Items Not Found!');
+    expect(toJSON(item)).toMatchSnapshot();
+  });
+});
--- components/SingleItem.js
+++ components/SingleItem.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
 import gql from 'graphql-tag';
-import Error from './ErrorMessage';
 import Head from 'next/head';
+import Error from './ErrorMessage';
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
-      item(
-        where: {
-          id: $id
-        }
-      ) {
-        id
+    item(where: { id: $id }) {
+      id
 
-        description
-        largeImage
-        title
-      }
+      description
+      largeImage
+      title
     }
-`
+  }
+`;
 
 const SingleItemStyle = styled.div`
   max-width: 1200px;
   margin: 2rem auto;
   // props and not this.props because <ThemeProvider passes theme in props
   // And it is not part of component
-  box-shadow: ${ props => props.theme.bs };
+  box-shadow: ${props => props.theme.bs};
   display: grid;
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
@@ -46,25 +42,28 @@ class SingleItem extends Component {
   render() {
     return (
       <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
-      {({loading, error, data}) => {
-        if (error) return <Error error={error} />
-        if (loading) return <p>Loading...</p>
-        if (!data.item) return <p> No item found for id {this.props.id}</p>
-        const item = data.item;
-        return <SingleItemStyle>
-          <Head>
-            <title>Dev Fits | {item.title}</title>
-          </Head>
-          <img src={item.largeImage} alt={item.title} />
-          <div className="details">
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-          </div>
-        </SingleItemStyle>
-      }}
-      </Query >
-    )
+        {({ loading, error, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p> No item found for id {this.props.id}</p>;
+          const { item } = data;
+          return (
+            <SingleItemStyle>
+              <Head>
+                <title>Dev Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyle>
+          );
+        }}
+      </Query>
+    );
   }
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
