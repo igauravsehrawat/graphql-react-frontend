01cc74ce00b3180a331e8e6a0c16d2a7b37e2525
G <igauravsehrawat@gmail.com>
2020-04-12 11:52:42 +0200

    --wip: RequestReset tests.

---
+++ __tests__/RequestReset.test.js
@@ -0,0 +1,47 @@
+import { mount } from 'enzyme';
+import wait from 'waait';
+import toJSON from 'enzyme-to-json';
+import Router from 'next/router';
+import { MockedProvider } from 'react-apollo/test-utils';
+import RequestReset, {
+  REQUEST_RESET_MUTATION,
+} from '../components/RequestReset';
+
+const mocks = [
+  {
+    request: {
+      query: REQUEST_RESET_MUTATION,
+      variables: { email: 'me@igauravsehrawat.com' },
+    },
+    result: {
+      data: { requestReset: { message: 'success', __typename: 'Message' } },
+    },
+  },
+];
+
+describe('<RequestReset/>', () => {
+  it('renders and matches snapshot', async () => {
+    const wrapper = mount(
+      <MockedProvider>
+        <RequestReset />
+      </MockedProvider>
+    );
+    const form = wrapper.find('form[data-test="form"]');
+    expect(toJSON(form)).toMatchSnapshot();
+  });
+
+  it('calls the mutation', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <RequestReset />
+      </MockedProvider>
+    );
+    wrapper.find('input').simulate('change', {
+      target: { name: 'email', value: 'root@igauravsehrawat.com' },
+    });
+    wrapper.find('form').simulate('submit');
+    await wait();
+    wrapper.update();
+    expect(wrapper.find('p').text()).toContain('Success, check your email!');
+  });
+});
--- components/RequestReset.js
+++ components/RequestReset.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
-import Form from './styles/Form';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
+import Form from './styles/Form';
 import Error from './ErrorMessage';
 
 const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
-    requestReset(
-      email: $email,
-    ) {
+    requestReset(email: $email) {
       message
     }
   }
@@ -17,9 +15,9 @@ const REQUEST_RESET_MUTATION = gql`
 export default class RequestReset extends Component {
   state = {
     email: '',
-  }
+  };
 
-  saveToState = (e) => {
+  saveToState = e => {
     const { name, value } = e.target;
     console.log({ name, value });
     this.setState({ [name]: value });
@@ -30,20 +28,28 @@ export default class RequestReset extends Component {
       <Mutation
         mutation={REQUEST_RESET_MUTATION}
         variables={{
-        ...this.state
+          ...this.state,
         }}
       >
-        {(requestReset, { data, error, loading }) => {
-          if (error) return <Error error={error} />
-          if (loading) return <p>Loading...</p>
+        {(requestReset, { data, error, loading, called }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
           return (
-            <Form method="post" onSubmit={async (e) => {
-              e.preventDefault();
-              await requestReset();
-              this.setState({ email: ''})
-            }}>
+            <Form
+              method="post"
+              onSubmit={async e => {
+                e.preventDefault();
+                await requestReset();
+                this.setState({ email: '' });
+              }}
+              data-test="form"
+            >
               <fieldset>
                 <p>Reset Password</p>
+                <Error error={error} />
+                {!loading && !error && called && (
+                  <p>Success, check your email!</p>
+                )}
                 <label htmlFor="email">email</label>
                 <input
                   id="email"
@@ -51,14 +57,17 @@ export default class RequestReset extends Component {
                   type="email"
                   value={this.state.email}
                   placeholder="Enter email"
-                  required={true}
+                  required
                   onChange={this.saveToState}
                 />
                 <button type="submit">Reset My Password</button>
               </fieldset>
-            </Form>)
+            </Form>
+          );
         }}
       </Mutation>
-    )
+    );
   }
 }
+
+export { REQUEST_RESET_MUTATION };
